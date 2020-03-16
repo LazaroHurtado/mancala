1d7d50dfd0b58d8153b04e2e746e209c87066dac
LazaroHurtado <hurtadolazaro12@gmail.com>
2020-03-16 17:13:19 -0400

    recoded whole logic

--- gameLogic.py
+++ gameLogic.py
@@ -1,80 +1,101 @@
 from node import Node
+from node import LinkedList
 
 class Mancala:
-	def __init__(self):
-		self.is_setup = False
-		if not self.is_setup:
-			self.set_up()
-			self.is_setup = True
-		self.show_board()
+    def __init__(self):
+        # All pits on the board
+        self.pits = ["A1", "A2", "A3", "A4", "A5", "A6", "P1", "B1", "B2", "B3", "B4", "B5", "B6", "P2"]
+        # Dictionary to hold pits and their node
+        self.pit_nodes = {}
+        # Turn pits into a node
+        self.create_nodes()
+        self.my_list = LinkedList()
+        self.create_linked_list()
+        self.show_board()
 
-	def show_board(self):
+    def show_board(self):
+        line_1 = "\n      B6 | B5 | B4 | B3 | B2 | B1       \n"
+        line_2 = "      "
+        B_pits = ["B6", "B5", "B4", "B3", "B2", "B1"]
 
-		line_1 = '       |  B6 |  B5 |  B4 |  B3 |  B2 |  B1 |       '
-		line_2_arr = ['B1','B2','B3','B4','B5','B6']
-		line_2 = '       |'
-		for i in range(6):
-			current_pit = line_2_arr.pop()
-			current_pit_val = str(self.pits_node[current_pit].get_val())
-			if len(current_pit_val) == 1:
-				line_2 += '  '+current_pit_val+'  |'
-			else:
-				line_2 += '  '+current_pit_val+' |'
-		line_2 += '       '
-		line_3 = '-------------------------------------'
-		for i in ['P1','P2']:
-			player_store = str(self.pits_node[i].get_val())
-			if i == 'P1':
-				if len(player_store) == 1:
-					line_3 += '  '+player_store+' P1'
-				else:
-					line_3 += '  '+player_store+' P1'
-			else:
-				if len(player_store) == 1:
-					line_3 = ' P2 '+player_store+'  '+line_3
-				else:
-					line_3 = ' P2 '+player_store+' '+line_3
-		line_4_arr = ['A6','A5','A4','A3','A2','A1']
-		line_4 = '       |'
-		for i in range(6):
-			current_pit = line_4_arr.pop()
-			current_pit_val = str(self.pits_node[current_pit].get_val())
-			if len(current_pit_val) == 1:
-				line_4 += '  '+current_pit_val+'  |'
-			else:
-				line_4 += '  '+current_pit_val+' |'
-		line_4 += '       '
-		line_5 = '       |  A1 |  A2 |  A3 |  A4 |  A5 |  A6 |       '
-		print(line_1+'\n'+line_2+'\n'+line_3+'\n'+line_4+'\n'+line_5)
+        for pit in B_pits:
+            pit_node = self.pit_nodes[pit]
+            pit_val = pit_node.get_val()
+            if pit_val > 9:
+                line_2 += str(pit_val)
+            else:
+                line_2 += "0"+str(pit_val)
+            if pit != B_pits[-1]:
+                line_2 += " | "
+            else:
+                line_2 += "      \n"
 
-	def set_up(self):
-		pits = ['B6','B5','B4','B3','B2','B1','P1','A6','A5','A4','A3','A2','A1','P2']
-		self.pits_node = {}
-		for i in pits:
-			if i in ['P1','P2']:
-				self.pits_node[i] = Node(i, 0)
-			else:
-				self.pits_node[i] = Node(i, 4)
-		last_pit = self.pits_node[pits[-1]]
-		while len(pits) > 1:
-			current_pit = pits.pop()
-			current_pit_node = self.pits_node[current_pit]
-			current_pit_node.set_next(self.pits_node[pits[-1]])
-		self.pits_node[pits.pop()].set_next(last_pit)
+        P2_node = self.pit_nodes["P2"]
+        P2_val = P2_node.get_val()
+        P1_node = self.pit_nodes["P1"]
+        P1_val = P1_node.get_val()
+        line_3 = "P2 "+ (str(P2_val) if P2_val > 9 else "0"+str(P2_val))+" --------------------------- "+(str(P1_val) if P1_val > 9 else "0"+str(P1_val))+" P1\n"
+        line_4 = "      "
+        A_pits = ["A1", "A2", "A3", "A4", "A5", "A6"]
 
-	def player_pit(self, pit, player):
-		if player == 'A':
-			not_store = 'P2'
-		else:
-			not_store = 'P1'
-		pit_value = self.pits_node[pit].get_val()
-		current_pit = pit
-		current_pit_node = self.pits_node[pit]
-		current_pit_node.add_val(-pit_value)
-		next_pit = current_pit_node.get_next()
-		for i in range(pit_value):
-			if next_pit.get_pit() == not_store:
-				next_pit = next_pit.get_next()
-			next_pit.add_val(1)
-			next_pit = next_pit.get_next()
-		self.show_board()
+        for pit in A_pits:
+            node = self.pit_nodes[pit]
+            val = node.get_val()
+            if val > 9:
+                line_4 += str(val)
+            else:
+                line_4 += "0"+str(val)
+            if pit != A_pits[-1]:
+                line_4 += " | "
+            else:
+                line_4 += "      \n"
+
+        line_5 = "      A1 | A2 | A3 | A4 | A5 | A6      \n"
+        print(line_1+line_2+line_3+line_4+line_5)
+
+    def create_nodes(self):
+        for pit in self.pits:
+            if pit == "P1" or pit == "P2":
+                current_pit = Node(pit, 0)
+            else:
+                current_pit = Node(pit, 4)
+            self.pit_nodes[pit] = current_pit
+
+    def create_linked_list(self):
+        for pit in self.pits:
+            node = self.pit_nodes[pit]
+            self.my_list.set_next(node)
+
+    def player_pit(self, pit, player):
+        node = self.pit_nodes[pit]
+        val = node.get_val()
+        node.pick_up()
+        cur = node.next
+        to_add = []
+
+        for i in range(val):
+            to_add.append(cur)
+            cur = cur.next
+        self.add_pits(to_add)
+
+        final = to_add[-1]
+        final_pit = final.get_pit()
+        player_pits = [("1", "A"), ("2", "B")]
+
+        if final_pit in ["P1", "P2"]:
+            if (final_pit[1], player) in player_pits:
+                self.show_board()
+                return True
+        self.show_board()
+        return False
+
+    def add_pits(self, pit_arr):
+        for pit in pit_arr:
+            pit.add_val(1)
+
+    def valid_pit(self, pit, player):
+        if pit not in self.pits or pit[0] != player:
+            return False
+        node = self.pit_nodes[pit]
+        val = node.get_val()
+        return val > 0
--- node.py
+++ node.py
@@ -1,31 +1,32 @@
 
 class Node:
-	def __init__(self, pit, val):
-		self.pit = pit
-		self.val = val
-		self.next = None
+    def __init__(self, pit, val):
+        self.pit = pit
+        self.val = val
+        self.next = None
 
-	def get_val(self):
-		return self.val
+    def get_val(self):
+        return self.val
 
-	def add_val(self, num):
-		self.val += num
+    def add_val(self, num):
+        self.val += num
 
-	def pick_up(self):
-		self.val = 0
+    def pick_up(self):
+        self.val = 0
 
-	def set_next(self, next_node):
-		self.next = next_node
-
-	def get_next(self):
-		return self.next
-
-	def get_pit(self):
-		return self.pit
+    def get_pit(self):
+        return self.pit
 
 class LinkedList:
-	def __init__(self, node):
-		self.node = node
+    def __init__(self):
+        self.tail = None
 
-	def get_next(self):
-		return self.node.next
+    def set_next(self, node):
+        if not self.tail:
+            self.tail = node
+            self.tail.next = node
+            return
+        cur = self.tail
+        node.next = cur.next
+        cur.next = node
+        self.tail = node
