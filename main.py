916d997e6cbfd49216dd7319e1d0be7ddd769565
LazaroHurtado <hurtadolazaro12@gmail.com>
2020-03-24 21:39:09 -0400

    added comments

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 from gameLogic import Mancala
 
+# main method for when playing the game
 def playGame():
+    # player one's name
     player_one = input('>> Player one name: ')
+    # player two's name
     player_two = input('>> Player two name: ')
+    # asign the pit letters to each player
     player_pits = {player_one:'A', player_two:'B'}
+    # instantiate the main game logic class
     gameClass = Mancala()
+    # keep track of the current player
     current_player = player_one
+    # infinite loop for playing the game
     while True:
+        # print whos turn it is
         pit_choosen = input(f">> {current_player}'s turn: ")
+        # get the pit letter of the current player
         player_letter = player_pits[current_player]
-
+        # if the pit choosen is valid play it
         if gameClass.valid_pit(pit_choosen, player_letter):
+            # pass the stones of the choosen pit around and check if the player
+            # should go again
             go_again = gameClass.player_pit(pit_choosen, player_letter)
+            # if the player doesn't go again then change the current player else don't
             if not go_again:
+                # change current player
                 current_player = (player_one) if current_player == player_two else player_two
-
-
+        # if the pit is not valid print error statement
         else:
             print('>> Invalid option, try again')
 
-
+# starting statements
 print('>> Welcome to Mancala!')
 print(">> To start enter 'start_game'")
 print(">> For game instruction enter 'game_help'")
 
+# get user input
 userInput = input('>> ')
 
+# start game
 if userInput == 'start_game':
     playGame()
--- node.py
+++ node.py
@@ -1,32 +1,48 @@
-
+# node class
 class Node:
     def __init__(self, pit, val):
+        # pit string
         self.pit = pit
+        # pit value
         self.val = val
+        # next pit in counter clockwise direction
         self.next = None
 
+    # get value of pit
     def get_val(self):
         return self.val
 
+    # add to pit value
     def add_val(self, num):
         self.val += num
 
+    # set pit value to 0
     def pick_up(self):
         self.val = 0
 
+    # get pit string
     def get_pit(self):
         return self.pit
 
-class LinkedList:
+# circular linked list class
+class CircularLinkedList:
     def __init__(self):
+        # last pit
         self.tail = None
 
+    # add pit to CLL
     def set_next(self, node):
+        # if there is no tail set tail to pass node
         if not self.tail:
             self.tail = node
+            # set tail node to point to itself
             self.tail.next = node
             return
+        # get current tail
         cur = self.tail
+        # set new node to point to what tail was pointing
         node.next = cur.next
+        # set tail to point to new node
         cur.next = node
+        # set tail to new node
         self.tail = node
